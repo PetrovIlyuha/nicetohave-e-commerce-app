fa9be26025c65614d016018bd5c4e4b9622a150c
Ilya <dnamix1@gmail.com>
2020-11-27 18:55:41 +0300

    admin dashboard

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import AdminNavSidebar from '../../components/navigation/AdminNavSidebar';
 
 const AdminDashboard = () => {
   return (
-    <div className='container'>
-      <div className='col col-md-2'>
-        <div className='row'>
-          <h2>Admin Dashboard</h2>
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col-md-3'>
+          <AdminNavSidebar />
+        </div>
+        <div className='col-md-8 offset-md-1'>
+          <div className='container mt-3'>
+            <h2>Admin Dashboard</h2>
+          </div>
         </div>
       </div>
     </div>
