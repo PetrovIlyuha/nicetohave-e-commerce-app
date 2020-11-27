e9c48a636126ea1a1c2f112132a1a50915b9a1a4
Ilya <dnamix1@gmail.com>
2020-11-27 18:51:59 +0300

    add image field to mongoose model, add theme reducer, default image if not found for the category

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
 import categoriesReducer from './categories/categoriesSlice';
+import themeReducer from './theme/themeSlice';
 
 export default combineReducers({
   user: userReducer,
   categories: categoriesReducer,
+  theme: themeReducer,
 });
