11f846a14fc8a5840832c454600a6ad2a669f671
Ilya <dnamix1@gmail.com>
2020-12-15 17:07:29 +0300

    externalized colors dictionary object and adjusted framer-motion animations

---
+++ client/src/pages/admin/product/productColors.js
@@ -0,0 +1,11 @@
+export const productColors = {
+  black: '#03042E',
+  white: '#E7E7E8',
+  silver: '#E3E3FF',
+  gold: '#FFFFE3',
+  red: '#C13531',
+  blue: '#0D41E1',
+  green: '#86B241',
+  orange: '#CF8542',
+  brown: '#5D3A2C',
+};
