800239d43301d171fc72fcaf21bff20523b43e45
Ilya <dnamix1@gmail.com>
2020-12-16 17:44:48 +0300

    added enpoint for all products GET/api/products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { adminRoleCheck, authCheck } from '../middlewares/auth.js';
-import { createProduct } from '../controllers/productController.js';
+import {
+  createProduct,
+  getAllProducts,
+} from '../controllers/productController.js';
 
 const router = express.Router();
 
 // routes
 router.post('/product', authCheck, adminRoleCheck, createProduct);
-
+router.get('/products', getAllProducts);
 export default router;
