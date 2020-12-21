3cb9c54a06faf792d4278b3b0f4c915105f48eb5
Ilya <dnamix1@gmail.com>
2020-12-21 13:14:03 +0300

    cloudinary setup to upload resized images

--- backend/controllers/cloudinaryController.js
+++ backend/controllers/cloudinaryController.js
@@ -1,20 +1,27 @@
 import cloudinary from 'cloudinary';
+import dotenv from 'dotenv';
 
-cloudinary.config({
+dotenv.config();
+
+cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export const uploadImages = async (req, res) => {
-  let result = await cloudinary.uploader.upload(req.body.image, {
-    public_id: `${Date.now()}`,
-    resource_type: 'auto', // jpeg/png
-  });
-  res.json({
-    public_id: result.public_id,
-    url: result.secure_url,
-  });
+  try {
+    let result = await cloudinary.v2.uploader.upload(req.body.image, {
+      public_id: `${Date.now()}`,
+      resource_type: 'auto', // jpeg/png
+    });
+    res.json({
+      public_id: result.public_id,
+      url: result.secure_url,
+    });
+  } catch (err) {
+    console.log('Cloudinary error', err);
+  }
 };
 
 export const removeImage = (req, res) => {
