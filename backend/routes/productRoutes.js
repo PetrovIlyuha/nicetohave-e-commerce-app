20a683aaef8ac309f40bd1fcbb5b5a4e2452854e
Ilya <dnamix1@gmail.com>
2020-12-22 18:36:57 +0300

    add routes for product by count && product by category for easy filtering on the product management admin page

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,11 +3,13 @@ import { adminRoleCheck, authCheck } from '../middlewares/auth.js';
 import {
   createProduct,
   getAllProducts,
+  getProductsByCategoryId,
 } from '../controllers/productController.js';
 
 const router = express.Router();
 
 // routes
 router.post('/product', authCheck, adminRoleCheck, createProduct);
-router.get('/products', getAllProducts);
+router.get('/products/:count', getAllProducts);
+router.get('/products-by-category/:id', getProductsByCategoryId);
 export default router;
