7e64025ae72abced9a6cd9f107c27bceb26acd11
Ilya <dnamix1@gmail.com>
2020-12-24 16:29:59 +0300

    extended main page styled component reckoning the theme change

--- client/src/styles/HomePageStyles.js
+++ client/src/styles/HomePageStyles.js
@@ -2,6 +2,12 @@ import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
 export const MainPageContainer = styled(motion.main)`
-  width: 80%;
-  margin: 50px auto;
+  width: 100%;
+  margin: 0 auto;
+  padding: 2rem 10rem;
+  background: ${props => (props.darkMode ? '#432371' : '#F0F3F6')};
+  text-align: center;
+  h2 {
+    color: ${props => (props.darkMode ? '#F0F3F6' : '#432371')};
+  }
 `;
