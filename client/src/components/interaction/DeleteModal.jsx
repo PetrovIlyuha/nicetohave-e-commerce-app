064f5412650bfbf82bca7d7e190e238020a539b6
Ilya <dnamix1@gmail.com>
2020-12-28 11:04:50 +0300

    delete modal gained ability to manage product deletion in it's deleteAndUpdate versatile function

--- client/src/components/interaction/DeleteModal.jsx
+++ client/src/components/interaction/DeleteModal.jsx
@@ -5,6 +5,7 @@ import 'react-responsive-modal/styles.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteOneCategoryBySlugThunk } from '../../redux/categories/categoriesSlice';
 import { deleteOneSubCategoryBySlug } from '../../redux/subcategories/subCategoriesSlice';
+import { deleteProductById } from '../../redux/product/productSlice';
 
 const { Meta } = Card;
 
@@ -19,12 +20,13 @@ const DeleteModal = ({
   const {
     user: { token },
   } = useSelector(state => state.user);
-
   const deleteAndUpdateItems = async () => {
     if (title === 'Category') {
       await dispatch(deleteOneCategoryBySlugThunk(item.slug, token));
-    } else {
+    } else if (title === 'Subcategory') {
       await dispatch(deleteOneSubCategoryBySlug(item.slug, token));
+    } else if (title === 'Product') {
+      await dispatch(deleteProductById(item._id, token));
     }
     setTimeout(() => onModalClose(), 40);
     updateItems(true);
@@ -39,8 +41,8 @@ const DeleteModal = ({
       <Card
         hoverable
         style={{ width: 240 }}
-        cover={<img alt='category visual' src={item.image} />}>
-        <Meta title='action' description={`Remove The ${title}?`} />
+        cover={<img alt='category visual' src={item.images[0].url} />}>
+        <Meta title={item.title} description={`Remove The ${title}?`} />
         <Divider orientation='center' style={{ fontSize: 10 }}>
           Confirm or decline
         </Divider>
