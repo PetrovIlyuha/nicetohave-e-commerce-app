add56074f5f39599809990ab03521b9bcf293929
Ilya <dnamix1@gmail.com>
2020-12-29 17:39:27 +0300

    added conditional render of menu as drawer based on media query

--- client/src/components/navigation/AdminNavSidebar.jsx
+++ client/src/components/navigation/AdminNavSidebar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Drawer } from 'antd';
 import {
   AiOutlineDashboard,
   AiTwotoneContainer,
@@ -11,6 +12,7 @@ import { GiPresent } from 'react-icons/gi';
 import { MdSecurity } from 'react-icons/md';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { useWindowWidth } from '../../hooks/useWindowWidth';
 
 const StyledNav = styled.nav`
   height: ${props => (props.fullHeight ? '100vh' : '100%')};
@@ -22,11 +24,97 @@ const StyledNav = styled.nav`
   color: 'white';
 `;
 
-const AdminNavSidebar = ({ fullHeight }) => {
+const AdminNavSidebar = ({ fullHeight, showSidebar, setShowSidebar }) => {
   const { darkMode: darkState } = useSelector(state => state.theme);
   const { categories } = useSelector(state => state.categories);
+  const [showDrawer, setShowDrawer] = useState(false);
+  const width = useWindowWidth();
 
-  return (
+  useEffect(() => {
+    if (window.matchMedia('(max-width: 770px)').matches) {
+      setShowDrawer(true);
+    } else {
+      setShowDrawer(false);
+    }
+  }, [width]);
+
+  const handleClose = e => {
+    setShowSidebar(false);
+  };
+
+  return showDrawer ? (
+    <Drawer
+      title='Navigation'
+      placement='left'
+      closable={true}
+      onClose={handleClose}
+      visible={showSidebar}
+      key={'side menu'}>
+      <StyledNav darkState={darkState} fullHeight={fullHeight}>
+        <ul className='nav flex-column justify-center'>
+          <li className='nav-item'>
+            <Link
+              to='/admin/dashboard'
+              className='nav-link'
+              style={{ color: darkState ? '#E0CBA8' : 'white' }}>
+              <AiOutlineDashboard /> Dashboard
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link
+              to='/admin/product-management'
+              className='nav-link'
+              style={{ color: darkState ? '#E0CBA8' : 'white' }}>
+              <SiCodefactor /> Products Management
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link
+              to={`/admin/products/${categories[0].name}`}
+              className='nav-link'
+              style={{ color: darkState ? '#E0CBA8' : 'white' }}>
+              <AiTwotoneContainer /> Products Creation
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link
+              to='/admin/category'
+              className='nav-link'
+              style={{ color: darkState ? '#E0CBA8' : 'white' }}>
+              <AiFillDatabase /> Single Categories
+            </Link>
+          </li>
+
+          <li className='nav-item'>
+            <Link
+              to='/admin/subcategory/null'
+              className='nav-link'
+              style={{ color: darkState ? '#E0CBA8' : 'white' }}>
+              <AiFillGold /> Category Management
+            </Link>
+          </li>
+
+          <li className='nav-item'>
+            <Link
+              to='/admin/coupons'
+              className='nav-link'
+              style={{ color: darkState ? '#E0CBA8' : 'white' }}>
+              <GiPresent /> Coupons
+            </Link>
+          </li>
+
+          <li className='nav-item'>
+            <Link
+              to='/user/password'
+              className='nav-link'
+              style={{ color: darkState ? '#E0CBA8' : 'white' }}>
+              <MdSecurity /> Password Update
+            </Link>
+          </li>
+        </ul>
+      </StyledNav>
+    </Drawer>
+  ) : (
     <StyledNav darkState={darkState} fullHeight={fullHeight}>
       <ul className='nav flex-column justify-center'>
         <li className='nav-item'>
